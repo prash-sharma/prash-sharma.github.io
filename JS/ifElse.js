a65dc8d84bc8b198102144d2090da6d34fb29b21
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-29 00:40:57 +1100

    Create ifElse.js

---
+++ JS/ifElse.js
@@ -0,0 +1,17 @@
+if(false){
+     console.log("I'm truly in the IF block")
+} else{
+    console.log("I'm in ELSE block.")
+};
+
+var whoIsHere = "teacher";
+
+if (whoIsHere==='user'){
+    console.log("Hey hey user");
+} else if (whoIsHere==='teacher'){
+    console.log('Hello teacher');
+} else{
+    console.log("Verify your email.")
+}
+
+
