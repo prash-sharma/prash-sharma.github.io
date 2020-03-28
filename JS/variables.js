635a6ac2b5f955001a6f87846313392d334cb803
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-29 00:41:12 +1100

    Create variables.js

---
+++ JS/variables.js
@@ -0,0 +1,16 @@
+let name = 'Aa...';
+
+let score = 100;
+let bonus = 20;
+
+let totalScore = score + bonus; 
+
+console.log(totalScore);
+
+let firstName = 'Ye';
+firstName = 'Chun';
+
+
+let lastName = 'Honda';
+
+console.log(firstName + ' ' + lastName);
