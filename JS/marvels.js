711de2b0096b2c3b857c31c3b1c214764b955f65
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-29 23:51:52 +1100

    Create marvels.js

---
+++ JS/marvels.js
@@ -0,0 +1,56 @@
+// // Arrays
+
+// const superHeroes = ['Thor', 'Ironman', 'Spiderman', 'Thanos'];
+// superHeroes.push(1, 'Someone');
+// console.log(superHeroes);
+// console.log(superHeroes[0]);
+// console.log(superHeroes[superHeroes.length - 1]);
+// console.log(`Total superheroes ${superHeroes.length}`);
+
+
+
+const numbers = ['one', 'two', 'three', 'four'];
+console.log(numbers);
+
+
+// numbers[1] = 'replaced positin two';
+// console.log(numbers);
+
+// numbers.push('new val');
+// console.log(numbers);
+
+// numbers.pop();
+// console.log(numbers);
+
+
+// numbers.shift();
+// console.log(numbers);
+
+// console.log(numbers.unshift('a'));
+// console.log(numbers);
+
+numbers.slice(2, 0, "newvalue");
+console.log(numbers);
+
+numbers.splice(0, 0, 'newvalue');
+console.log(numbers);
+
+numbers.push('a', 'b');
+console.log(numbers);
+
+numbers.unshift('value 1');
+console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
