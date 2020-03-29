bda8af5fa77ba932fa25769be94fab169e4bff27
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-29 23:52:02 +1100

    Update ageindays.js

--- Shorts/AgeInDays/ageindays.js
+++ Shorts/AgeInDays/ageindays.js
@@ -1,14 +1,16 @@
 function ageInDays(){
     var birthYear = prompt("Year of born?");
-    var ageInDayz = (2020 - birthYear) * 365;    
-    var h1 = document.createElement('p');
-    var textAnswer = document.createTextNode('You are ' + ageInDayz + ' days old.');
-    h1.setAttribute('id', 'ageInDays');
-    h1.appendChild(textAnswer);
-    document.getElementById('flex-box-result').appendChild(h1);
+    var ageInDayz = (2020 - birthYear) * 365;
+
+    document.getElementById('flex-box-result').innerHTML = ('Days in number is:' + ageInDayz);
+
+    // var h1 = document.createElement('p');
+    // var textAnswer = document.createTextNode(ageInDayz);
+    // //h1.setAttribute('id', 'ageInDays');
+    // h1.appendChild(textAnswer);
+    // document.getElementById('flex-box-result').appendChild(h1);
 }
 
 function reset(){
     document.getElementById('flex-box-result').remove();
 }
-
