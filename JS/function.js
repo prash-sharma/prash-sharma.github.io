321caa21a00c86e59336d5623b771f6f7f10e6e5
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-30 13:19:44 +1100

    Update function.js

--- JS/function.js
+++ JS/function.js
@@ -1,25 +1,40 @@
-function sayHello(name, city){
-    console.log(`Greetings my friend ${name} from ${city}`);
-    console.log(`Hey ${name}`);
+// function sayHello(name, city){
+//     console.log(`Greetings my friend ${name} from ${city}`);
+//     console.log(`Hey ${name}`);
     
+// }
+
+// sayHello('John', 'Syd');
+
+
+// let fullNameMaker = function(firstName, lastName){
+//     console.log("Welcome bud");
+//     console.log(`Hey ${firstName} ${lastName}`)
+// }
+
+// fullNameMaker('Ye', 'Honda');
+
+// let myAdder = function (num1, num2) {
+//     let result = num1 + num2;
+//     return result;
+// }
+
+// myAdder(2, 5);
+// let total = myAdder(2, 2);
+// console.log(myAdder(1, 2));
+// console.log(total);
+
+let multiplier = function (num1, num2) {
+    return num1 * num2;
 }
 
-sayHello('John', 'Syd');
+console.log(multiplier(5, 5));
 
 
-let fullNameMaker = function(firstName, lastName){
-    console.log("Welcome bud");
-    console.log(`Hey ${firstName} ${lastName}`)
+let guestUser = function (name = 'Guest', courseCount = 0) {
+    return 'Hello ' + name +', your course count is: ' + courseCount;
 }
 
-fullNameMaker('Ye', 'Honda');
+console.log(guestUser('Ye', 10));
 
-let myAdder = function (num1, num2) {
-    let result = num1 + num2;
-    return result;
-}
-
-myAdder(2, 5);
-let total = myAdder(2, 2);
-console.log(myAdder(1, 2));
-console.log(total);
+console.log(guestUser());
