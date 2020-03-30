bd63e41287d815e5651d3433773c9421759fd4dc
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-30 13:19:47 +1100

    Create trello.js

---
+++ JS/trello.js
@@ -0,0 +1,21 @@
+const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun'];
+
+// console.log(days[1]);
+
+// for (i = 0; i < days.length; i++) {
+//     const element = days[i];
+//     console.log(element);
+// }
+
+// const toDos = [];
+
+// toDos.push('Eat');
+// toDos.push('Drink');
+// toDos.push('Sleep')
+// toDos.push('Repeat');
+
+// console.log(toDos.length);
+
+// toDos.splice(2,0, 'hey hey');
+
+// console.log(toDos);
