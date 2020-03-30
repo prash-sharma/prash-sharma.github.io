af84a32ce623ce2a10df4c1bedf52367cb08ecd5
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-30 19:17:50 +1100

    Update trello.js

--- JS/trello.js
+++ JS/trello.js
@@ -1,21 +1,63 @@
-const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun'];
+// const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun'];
 
-// console.log(days[1]);
+/*
+for (let i = 0; i < days.length; i++) {
+    const element = days[i];
+    console.log(element);   
+}
 
-// for (i = 0; i < days.length; i++) {
-//     const element = days[i];
-//     console.log(element);
-// }
+console.log(days[1]); 
+*/
 
-// const toDos = [];
+/*
+const toDos = [];
 
-// toDos.push('Eat');
-// toDos.push('Drink');
-// toDos.push('Sleep')
-// toDos.push('Repeat');
+toDos.push('Eat');
+toDos.push('Drink');
+toDos.push('Sleep')
+toDos.push('Repeat');
+
+toDos.forEach(function(item, index){
+    console.log(`${index+1} is ${item}`);
+    
+}) 
 
 // console.log(toDos.length);
 
 // toDos.splice(2,0, 'hey hey');
 
-// console.log(toDos);
+// console.log(toDos); 
+*/
+
+/*
+days.forEach(function(item, index){
+    let indexing = index+1;
+    console.log(`Day ${indexing} -- ${item}`);
+})
+*/
+
+/*
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dev'];
+
+months.forEach(function(item, index){
+    console.log(`Month ${index+1} is ${item}`);
+})
+
+const toDoList = ['eat', 'drink', 'sleep', 'repeat'];
+toDoList.forEach(function(item, index){
+    console.log(`${index+1}st item is ${item}`);
+})
+*/
+
+/*
+const sfChars = [];
+
+sfChars.unshift('Ryu', 'Ken', 'Bison', 'Guile');
+
+for (let i = 0; i < sfChars.length; i++) {
+    const element = sfChars[i];
+    console.log(element);
+} 
+*/
+
+
