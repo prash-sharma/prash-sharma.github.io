762fb243bc288d85c559951fa36f7fea1160d586
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-30 22:23:02 +1100

    Create trello2 copy.js

---
+++ JS/Intermediate/trello2 copy.js
@@ -0,0 +1,70 @@
+/*let myToDos = {
+    day: 'Mon',
+    meetings: 0,
+    meetDone: 0,
+}
+
+function addMeeting(todo, meet = 0){
+    todo.meetings = todo.meetings + meet;
+    
+}
+
+addMeeting(myToDos, 5); //meetings: 5, meetDone: 0
+console.log(myToDos);
+addMeeting(myToDos, 5);
+console.log(myToDos);
+
+
+function meetDone(todo, meet = 0){
+    todo.meetDone = meet;
+}
+
+meetDone(myToDos, 3)
+
+console.log(myToDos);
+
+let result = myToDos.meetings - myToDos.meetDone;
+console.log(`Meetings left for ${myToDos.day} is ${result}`);
+*/
+
+/*
+// function resetDay(todo) {
+//     todo.meetings = 0;
+//     todo.meetDone = 0;
+// }
+
+// function getSummaryOfDay(todo) {
+//     let meetLeft = todo.meetings + todo.meetDone;
+//     return `You've ${meetLeft} meetings left today`;
+// }
+
+// addMeeting(myToDos, 4);
+// addMeeting(myToDos, 2)
+// meetDone(myToDos, 5);
+
+// console.log(myToDos);
+
+// console.log(getSummaryOfDay(myToDos));
+*/
+
+
+// |||**************************|||
+
+
+//Create a list
+let meetingCount = {
+    day: 'Sun',
+    taskInitial: 0,
+    taskDone: 0
+}
+
+// Get the initial meetings total from the user
+
+function initialTaskTotal(params) {
+    
+}
+
+
+
+
+// Get the task completed from the user
