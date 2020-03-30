ee49eb8222d3604375a7d6b9d25c074d7eb278c0
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-30 19:17:48 +1100

    Create trello2.js

---
+++ JS/Intermediate/trello2.js
@@ -0,0 +1,17 @@
+let myToDos = {
+    day: 'Mon',
+    meetings: 0,
+    meetDone: 0,
+}
+
+function meetingTotal(todo, meet = 0){
+    return todo.meetings = todo.meetings + meet;
+    
+}
+
+// console.log(meetingTotal(myToDos, 3));
+
+
+function doneCount(done, meet = 0){
+    done.
+}
