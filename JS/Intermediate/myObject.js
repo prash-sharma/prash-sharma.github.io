42bb5ad6c2c84a1d18a12600e22fc326e5eca354
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-30 19:17:45 +1100

    Create myObject.js

---
+++ JS/Intermediate/myObject.js
@@ -0,0 +1,14 @@
+// let myVideos = {
+//     title: "Video name",
+//     length: "15",
+//     description: "My fav video"
+// }
+
+let myCourse = {
+    name: "JS Tutorial",
+    author: "Hitesh",
+    price: "Free"
+}
+
+console.log(`${myCourse.name} by ${myCourse.author} is ${myCourse.price}`);
+console.log(myCourse.name, myCourse.author, myCourse.price);
