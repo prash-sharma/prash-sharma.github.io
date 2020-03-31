82a75e538e682546daa1caf4887c11ec73992be3
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-31 22:29:29 +1100

    Create dice.js

---
+++ JS/Intermediate/dice.js
@@ -0,0 +1,23 @@
+// let numOne = 22;
+// let numTwo = 7;
+
+// result = (numOne/numTwo).toFixed(2);
+// x = Math.floor(result);
+// y = Math.ceil(result)
+// value = Math.PI;
+
+// console.log(result);
+// console.log(value);
+// console.log(Math.sqrt(16));
+// console.log(x);
+// console.log(y);
+
+
+let min = 1;
+let max = 6;
+
+let dice = Math.ceil(Math.random() * 6);
+
+console.log(dice);
+
+
