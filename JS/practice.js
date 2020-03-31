e433263e53b2c52f6ad46613f544bd1d900a5016
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-03-31 22:29:57 +1100

    Create practice.js

---
+++ JS/practice.js
@@ -0,0 +1,13 @@
+const championsLeague = [];
+
+championsLeague.push('Man Utd')
+championsLeague.push('AC Milan')
+championsLeague.push('PSG')
+championsLeague.push('Juventus')
+championsLeague.push('Arsenal')
+championsLeague.push('Man City')
+championsLeague.push('Spurs')
+championsLeague.push('Dortmund')
+championsLeague.push('Barca')
+
+console.log(championsLeague.length);
