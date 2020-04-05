5e8ce51ee179e29422d31b09f7feb9ba0b8ac885
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-04-05 14:56:13 +1000

    Create signup.js

---
+++ JS/Try/signup.js
@@ -0,0 +1,22 @@
+document.querySelector('.signup').addEventListener('submit', (event)=>{
+    event.preventDefault()
+    let p1 = event.target.password1.value
+    let p2 = event.target.password2.value
+    
+    validation = ()=>{
+    if (p1 != p2) {
+        return 'Passwords do not match'
+    } else {
+        return 'Get in!!'
+    }}
+    
+    let output = document.createElement('p')
+    output.textContent = validation()
+    document.querySelector('body').appendChild(output)
+    
+    event.target.name.value = ''
+    event.target.email.value = ''
+    event.target.password1.value = ''
+    event.target.password2.value = ''
+}
+)
