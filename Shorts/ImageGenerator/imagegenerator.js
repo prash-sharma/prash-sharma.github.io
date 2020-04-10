b4d510f2394a18de7db2dfbe22af85e66b456a9d
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-04-10 22:56:44 +1000

    Update imagegenerator.js

--- Shorts/ImageGenerator/imagegenerator.js
+++ Shorts/ImageGenerator/imagegenerator.js
@@ -1,13 +1,26 @@
 function imageGenerator(){
-    var image = document.createElement('img');
-    var abc = document.getElementById('flex-image-gen');
-    image.src = "https://api.thecatapi.com/api/images/get?format=src&type=gif&size=random";
-    abc.appendChild(image);
-}
+    let div = document.createElement('div');
+    div.className = "container-item-1"
+    div.id = "flex-image-gen"
+    document.querySelector('.container').appendChild(div)
+    
+// <div class="container-item-1" id="flex-image-gen">
 
-function myFunction(){
+    var imageSrc = document.createElement('img');
+    var abc = document.getElementById('flex-image-gen');
+    imageSrc.src = "https://api.thecatapi.com/api/images/get?format=src&type=gif&size=random";
+    abc.appendChild(imageSrc);
+
     var image = document.createElement('img');
-    image.src = "pp.jpg";
+    image.src = "lynel.jpg";
     var newDiv = document.getElementById('flex-image-gen');
     newDiv.appendChild(image);
+
+    let video = document.createElement('iFrame');
+    video.src = '/aanya.mp4';
+    document.getElementById('flex-image-gen').appendChild(video);
+}
+
+function reset(){
+    document.getElementById('flex-image-gen').remove()
 }
