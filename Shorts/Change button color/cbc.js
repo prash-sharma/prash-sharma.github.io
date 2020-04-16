960e72eff6a7b2dc8d72f3f16a0d0a82083ba045
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-04-16 12:29:50 +1000

    Update cbc.js

--- Shorts/Change button color/cbc.js
+++ Shorts/Change button color/cbc.js
@@ -7,15 +7,51 @@ let btnDefaultColor = document.getElementsByTagName('button');
 // Store all buttons' initial values into an Array 
 let copiedBtnDefaultColor = [];
 for (let index = 0; index < btnDefaultColor.length; index++) {
-    copiedBtnDefaultColor.push(btnDefaultColor[index]);
+    copiedBtnDefaultColor.push(btnDefaultColor[index].classList[1]);
 }
 // console.log(copiedBtnDefaultColor);
 
-function updateColor(selectProps){
-    if (selectProps.value === 'select') {
+function updateColor(selectedProps){
+    if (selectedProps.value === 'select') {
         return null
-    } else if (selectProps.value == 'green') {
-        btnGreen();
+    } else if (selectedProps.value === 'green') {
+        setNewColor('green')
+    } else if (selectedProps.value === 'blue') {
+        setNewColor('blue')
+    } else if (selectedProps.value === 'yellow') {
+        setNewColor('yellow')
+    } else if (selectedProps.value === 'red') {
+        setNewColor('red')
+    } else if (selectedProps.value === 'orange') {
+        setNewColor('orange')
+    } else if (selectedProps.value === 'random') {
+        setRandom()
+    } else {
+        setReset()
     }
+}
+
+function setNewColor(newColor){
+    for (let index = 0; index < btnDefaultColor.length; index++) {
+        btnDefaultColor[index].classList.remove(btnDefaultColor[index].classList[1])
+        btnDefaultColor[index].classList.add(newColor)
+    }
+}
+
+function setRandom(){
+    let randomColors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red']
+    // let i = Math.floor( Math.random() * 7)
+    // console.log(randomColors[i]);
     
+    for (let index = 0; index < btnDefaultColor.length; index++) {
+        btnDefaultColor[index].classList.remove(btnDefaultColor[index].classList[1])
+        btnDefaultColor[index].classList.add(randomColors[Math.floor( Math.random() * 7)])
+    }
+}
+
+function setReset(){
+    for (let index = 0; index < btnDefaultColor.length; index++) {
+        btnDefaultColor[index].classList.remove(btnDefaultColor[index].classList[1]);
+        btnDefaultColor[index].classList.add(copiedBtnDefaultColor[index])
+    }
 }
