33ed5d6931fe9028df5d3561005216d66b869ee8
prash-sharma <62488458+prash-sharma@users.noreply.github.com>
2020-04-28 02:52:17 +1000

    More updates

--- Shorts/TicTacToe/tictactoe.js
+++ Shorts/TicTacToe/tictactoe.js
@@ -1,298 +1,122 @@
-// alert ('Hello world');
-
-
-// function eventListener() {
-// for (let index = 1; index <= 9; index++) {
-//     let cellValue = `#cell${index}`
-//     document.querySelector(cellValue).addEventListener('click', `addInput${index}`, {once: true})
-    
-// }
-// }
-
-// const allCells = document.querySelectorAll('.cell').values;
-
-// console.log(allCells);
-// console.log(typeof(allCells));
-// allCells[0] = 'some new value'
-// console.log(allCells[0]);
-
-// function addInput(){
-//     if (player1){
-//         c1.textContent = 'X';
-//         changeCursor('o');
-//         player1 = false
-//     } else {
-//         c1.textContent = 'O';
-//         c1.style.color = 'red';
-//         changeCursor('x');
-//         player1 = true
-//     }
-    
-//     getWinner();
-// }
-
-
-// *************************************************
-
+document.querySelector('.board').style.cursor = "url(images/x.png), auto";
 
 let player1 = true
 let winner
 
-document.querySelector('.board').style.cursor = "url(images/x.png), auto";
+const CELLS = document.querySelectorAll('.cell');
 
-let c1 = document.getElementById('cell1')
-let c2 = document.getElementById('cell2')
-let c3 = document.getElementById('cell3')
-let c4 = document.getElementById('cell4')
-let c5 = document.getElementById('cell5')
-let c6 = document.getElementById('cell6')
-let c7 = document.getElementById('cell7')
-let c8 = document.getElementById('cell8')
-let c9 = document.getElementById('cell9')
+CELLS.forEach(eventListener);
 
+function eventListener(item){
+    item.addEventListener('click', addInput, {once: true})
+}
 
+function addInput(event){
+    
+    let clickedId = (parseInt(event.target.id) - 1);
 
-document.querySelector('#cell1').addEventListener('click', addInput1, {once: true})
-document.querySelector('#cell2').addEventListener('click', addInput2, {once: true})
-document.querySelector('#cell3').addEventListener('click', addInput3, {once: true})
-document.querySelector('#cell4').addEventListener('click', addInput4, {once: true})
-document.querySelector('#cell5').addEventListener('click', addInput5, {once: true})
-document.querySelector('#cell6').addEventListener('click', addInput6, {once: true})
-document.querySelector('#cell7').addEventListener('click', addInput7, {once: true})
-document.querySelector('#cell8').addEventListener('click', addInput8, {once: true})
-document.querySelector('#cell9').addEventListener('click', addInput9, {once: true})
-
-
-function addInput1(){
+    console.log(clickedId);
     
     if (player1){
-        c1.textContent = 'X';
+        CELLS[clickedId].textContent = 'X';
         changeCursor('o');
         player1 = false
     } else {
-        c1.textContent = 'O';
-        c1.style.color = 'red';
+        CELLS[clickedId].textContent = 'O';
+        CELLS[clickedId].style.color = 'red';
         changeCursor('x');
         player1 = true
     }
     
-    getWinner();
+    // getWinner();
 }
 
 
-function addInput2(){
+
+
+// // GET WINNER
+// function getWinner(){
     
-    if (player1){
-        c2.textContent = 'X';
-        changeCursor('o');
-        player1 = false
-    } else {
-        c2.textContent = 'O';
-        c2.style.color = 'red';
-        changeCursor('x');
-        player1 = true
-    }
+//     if ( (c1.innerText =='X' && c2.innerText == 'X' && c3.innerText == 'X') || 
+//          (c4.innerText =='X' && c5.innerText == 'X' && c6.innerText == 'X') || 
+//          (c7.innerText =='X' && c8.innerText == 'X' && c9.innerText == 'X') || 
 
-    getWinner();
-}
+//          (c1.innerText =='X' && c4.innerText == 'X' && c7.innerText == 'X') ||
+//          (c2.innerText =='X' && c5.innerText == 'X' && c8.innerText == 'X') ||
+//          (c3.innerText =='X' && c6.innerText == 'X' && c9.innerText == 'X') ||
 
-
-function addInput3(){
-    
-    if (player1){
-        c3.textContent = 'X';
-        changeCursor('o');
-        player1 = false
-    } else {
-        c3.textContent = 'O';
-        c3.style.color = 'red';
-        changeCursor('x');
-        player1 = true
-    }
-
-    getWinner();
-}
-
-function addInput4(){
-    
-    if (player1){
-        c4.textContent = 'X';
-        changeCursor('o');
-        player1 = false
-    } else {
-        c4.textContent = 'O';
-        c4.style.color = 'red';
-        changeCursor('x');
-        player1 = true
-    }
-
-    getWinner();
-}
-
-function addInput5(){
-    
-    if (player1){
-        c5.textContent = 'X';
-        changeCursor('o');
-        player1 = false
-    } else {
-        c5.textContent = 'O';
-        c5.style.color = 'red';
-        changeCursor('x');
-        player1 = true
-    }
-
-    getWinner();
-}
-
-function addInput6(){
-    
-    if (player1){
-        c6.textContent = 'X';
-        changeCursor('o');
-        player1 = false
-    } else {
-        c6.textContent = 'O';
-        c6.style.color = 'red';
-        changeCursor('x');
-        player1 = true
-    }
-
-    getWinner();
-}
-
-function addInput7(){
-    
-    if (player1){
-        c7.textContent = 'X';
-        changeCursor('o');
-        player1 = false
-    } else {
-        c7.textContent = 'O';
-        c7.style.color = 'red';
-        changeCursor('x');
-        player1 = true
-    }
-    
-    getWinner();
-}
-
-function addInput8(){
-    
-    if (player1){
-        c8.textContent = 'X';
-        changeCursor('o');
-        player1 = false
-    } else {
-        c8.textContent = 'O';
-        c8.style.color = 'red';
-        changeCursor('x');
-        player1 = true
-    }
-
-    getWinner();
-}
-
-function addInput9(){
-    
-    if (player1){
-        c9.textContent = 'X';
-        changeCursor('o');
-        player1 = false
-    } else {
-        c9.textContent = 'O';
-        c9.style.color = 'red';
-        changeCursor('x');
-        player1 = true
-    }
-
-    getWinner();
-}
-
-
-// GET WINNER
-function getWinner(){
-    
-    if ( (c1.innerText =='X' && c2.innerText == 'X' && c3.innerText == 'X') || 
-         (c4.innerText =='X' && c5.innerText == 'X' && c6.innerText == 'X') || 
-         (c7.innerText =='X' && c8.innerText == 'X' && c9.innerText == 'X') || 
-
-         (c1.innerText =='X' && c4.innerText == 'X' && c7.innerText == 'X') ||
-         (c2.innerText =='X' && c5.innerText == 'X' && c8.innerText == 'X') ||
-         (c3.innerText =='X' && c6.innerText == 'X' && c9.innerText == 'X') ||
-
-         (c1.innerText =='X' && c5.innerText == 'X' && c9.innerText == 'X') ||
-         (c3.innerText =='X' && c5.innerText == 'X' && c7.innerText == 'X') ) {
+//          (c1.innerText =='X' && c5.innerText == 'X' && c9.innerText == 'X') ||
+//          (c3.innerText =='X' && c5.innerText == 'X' && c7.innerText == 'X') ) {
             
-        winner = 'X';
+//         winner = 'X';
         
-        document.querySelector('#cell1').removeEventListener('click', addInput1);
+//         document.querySelector('#cell1').removeEventListener('click', addInput1);
 
-        document.querySelector('#cell2').removeEventListener('click', addInput2);
+//         document.querySelector('#cell2').removeEventListener('click', addInput2);
 
-        document.querySelector('#cell3').removeEventListener('click', addInput3);
+//         document.querySelector('#cell3').removeEventListener('click', addInput3);
 
-        document.querySelector('#cell4').removeEventListener('click', addInput4);
+//         document.querySelector('#cell4').removeEventListener('click', addInput4);
 
-        document.querySelector('#cell5').removeEventListener('click', addInput5);
+//         document.querySelector('#cell5').removeEventListener('click', addInput5);
 
-        document.querySelector('#cell6').removeEventListener('click', addInput6);
+//         document.querySelector('#cell6').removeEventListener('click', addInput6);
 
-        document.querySelector('#cell7').removeEventListener('click', addInput7);
+//         document.querySelector('#cell7').removeEventListener('click', addInput7);
 
-        document.querySelector('#cell8').removeEventListener('click', addInput8);
+//         document.querySelector('#cell8').removeEventListener('click', addInput8);
 
-        document.querySelector('#cell9').removeEventListener('click', addInput9);
+//         document.querySelector('#cell9').removeEventListener('click', addInput9);
 
-        console.log('X wins');
-        
-        
-        
-
-    } else if ( (c1.innerText =='O' && c2.innerText == 'O' && c3.innerText == 'O') || 
-                (c4.innerText =='O' && c5.innerText == 'O' && c6.innerText == 'O') || 
-                (c7.innerText =='O' && c8.innerText == 'O' && c9.innerText == 'O') || 
-
-                (c1.innerText =='O' && c4.innerText == 'O' && c7.innerText == 'O') ||
-                (c2.innerText =='O' && c5.innerText == 'O' && c8.innerText == 'O') ||
-                (c3.innerText =='O' && c6.innerText == 'O' && c9.innerText == 'O') ||
-
-                (c1.innerText =='O' && c5.innerText == 'O' && c9.innerText == 'O') ||
-                (c3.innerText =='O' && c5.innerText == 'O' && c7.innerText == 'O')) {
-        
-        winner = 'O';
-
-        document.querySelector('#cell1').removeEventListener('click', addInput1);
-
-        document.querySelector('#cell2').removeEventListener('click', addInput2);
-
-        document.querySelector('#cell3').removeEventListener('click', addInput3);
-
-        document.querySelector('#cell4').removeEventListener('click', addInput4);
-
-        document.querySelector('#cell5').removeEventListener('click', addInput5);
-
-        document.querySelector('#cell6').removeEventListener('click', addInput6);
-
-        document.querySelector('#cell7').removeEventListener('click', addInput7);
-
-        document.querySelector('#cell8').removeEventListener('click', addInput8);
-
-        document.querySelector('#cell9').removeEventListener('click', addInput9);
-
-        console.log('Zero wins');
+//         console.log('X wins');
         
         
-    } 
+        
 
-    if (winner == 'X' || winner == 'O'){
-        document.querySelector('.winner').textContent = (`Winner is: ${winner}`);
-        document.querySelector('.board').style.cursor = "not-allowed";
-    } else {
-        document.querySelector('.winner').textContent = (`It's a draw`);
-    }
+//     } else if ( (c1.innerText =='O' && c2.innerText == 'O' && c3.innerText == 'O') || 
+//                 (c4.innerText =='O' && c5.innerText == 'O' && c6.innerText == 'O') || 
+//                 (c7.innerText =='O' && c8.innerText == 'O' && c9.innerText == 'O') || 
+
+//                 (c1.innerText =='O' && c4.innerText == 'O' && c7.innerText == 'O') ||
+//                 (c2.innerText =='O' && c5.innerText == 'O' && c8.innerText == 'O') ||
+//                 (c3.innerText =='O' && c6.innerText == 'O' && c9.innerText == 'O') ||
+
+//                 (c1.innerText =='O' && c5.innerText == 'O' && c9.innerText == 'O') ||
+//                 (c3.innerText =='O' && c5.innerText == 'O' && c7.innerText == 'O')) {
+        
+//         winner = 'O';
+
+//         document.querySelector('#cell1').removeEventListener('click', addInput1);
+
+//         document.querySelector('#cell2').removeEventListener('click', addInput2);
+
+//         document.querySelector('#cell3').removeEventListener('click', addInput3);
+
+//         document.querySelector('#cell4').removeEventListener('click', addInput4);
+
+//         document.querySelector('#cell5').removeEventListener('click', addInput5);
+
+//         document.querySelector('#cell6').removeEventListener('click', addInput6);
+
+//         document.querySelector('#cell7').removeEventListener('click', addInput7);
+
+//         document.querySelector('#cell8').removeEventListener('click', addInput8);
+
+//         document.querySelector('#cell9').removeEventListener('click', addInput9);
+
+//         console.log('Zero wins');
+        
+        
+//     } 
+
+//     if (winner == 'X' || winner == 'O'){
+//         document.querySelector('.winner').textContent = (`Winner is: ${winner}`);
+//         document.querySelector('.board').style.cursor = "not-allowed";
+//     } else {
+//         document.querySelector('.winner').textContent = (`It's a draw`);
+//     }
    
-}
+// }
 
 
 
@@ -310,5 +134,6 @@ function changeCursor(player){
     }
     
 }
+
 
 
