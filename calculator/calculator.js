5ba4fb57c94cdc76dd82e65cad90e81988c773f2
Prash Sharma <st.prashanta@gmail.com>
2020-05-13 18:30:42 +1000

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,14 +6,50 @@ const equalsButton = document.querySelector('.equals');
 const results = document.querySelector('.results');
 
 
-inputButtons.forEach(eventListener);
 
-function eventListener(item){
+// NUMBER BUTTONS
+inputButtons.forEach(numberEventListener);
+
+function numberEventListener(item){
     item.addEventListener('click', (e) => {
         
         let inputVal = e.target.innerText;
         console.log(inputVal);
-        
-        results.textContent = results.textContent + inputVal;
+        if (inputVal === '.' && results.textContent.includes('.')){
+            return
+        } else {
+            results.textContent = results.textContent + inputVal;
+        }
     })
 };
+
+
+// DELETE BUTTON
+delButton.addEventListener('click', () => results.textContent = results.textContent.substring(0, results.textContent.length-1));
+
+
+// AC BUTTON
+acButton.addEventListener('click', () => results.textContent = '');
+
+
+// OPERATORS
+
+operatorButtons.forEach(operatorEventListener);
+
+console.log(operatorButtons);
+
+
+function operatorEventListener(item){
+    item.addEventListener('click', addOperator);
+
+    function addOperator(e){
+        
+
+        if (results.textContent.includes(e.operator)){
+            return
+        } else{
+            results.textContent += e.target.textContent;
+        }
+
+    }
+}
