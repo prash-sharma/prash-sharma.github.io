bad92af505441c86d99b4e33f6a6dc40c4028dd0
Prash Sharma <st.prashanta@gmail.com>
2020-05-22 18:09:22 +1000

    cors

--- news/news.js
+++ news/news.js
@@ -1,1 +1,16 @@
-// 96d7d1d2511b490e94edaa7f7c10d895
+
+async function getData(){
+    const url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=e9d3a9efed9545d486a9367a64d54173';
+
+    let response = await fetch(url);
+    let data = await response.json();
+    return data
+}
+let a = getData();
+
+a.then((data)=>{
+    console.log(data);
+    
+}).catch((err) => {
+    console.log(err);
+})
--- practice.js
+++ practice.js
@@ -29,7 +29,8 @@ myBtn.addEventListener('click', trigGetData);
 // }
 
 async function getData(){
-    let url = `animals.json`
+    const url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=e9d3a9efed9545d486a9367a64d54173';
+    
     let response = await fetch(url);
     
     let data = await response.json();
