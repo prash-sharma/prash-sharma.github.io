7dbdc00348aa4c03743220987d6ca70d352d4b93
Prash Sharma <st.prashanta@gmail.com>
2020-05-26 19:03:24 +1000

    fetch

--- postIt/postIt.js
+++ postIt/postIt.js
@@ -1,7 +1,9 @@
 console.log('PostIt');
 
-let url = document.querySelector('.url');
+// let url = document.querySelector('.url');
+let url = 'https://randomuser.me/api/?results=5';
 let submitBtn = document.querySelector('.submit');
+let result = document.querySelector('#res');
 
 
 // let requestType = document.querySelector('#get');
@@ -10,9 +12,33 @@ let submitBtn = document.querySelector('.submit');
 
 submitBtn.addEventListener('click', () => {
     let reqType = document.querySelector(`input[name='reqType']:checked`);
-    console.log(url.value);
-    console.log(reqType.value);
+    // console.log(url.value);
+   
+
+    getData(url, reqType).then((data) => {
+        displayData(data);
+         console.log(reqType.value);
+    }).catch((err) => {
+        console.log(err.message);
+        console.log(reqType.value);
+    })
     
 });
+
+
+async function getData(url, method){
+    let response = await fetch(url, method);
+    let data = await response.json();
+    return data;
+}
+
+
+function displayData(data){
+    console.log(data);
+        result.value = JSON.stringify(data);
+        console.log(result);
+    }
+    
+
 
 
