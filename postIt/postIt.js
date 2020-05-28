f59a769401b6e527bf020652cf57af70b880f76f
Prash Sharma <st.prashanta@gmail.com>
2020-05-28 15:06:59 +1000

    post

--- postIt/postIt.js
+++ postIt/postIt.js
@@ -29,64 +29,86 @@ document.querySelector('.btn-add').addEventListener('click', () => {
 })
 
 
-
-
 let loader = document.querySelector('.loader');
     loader.hidden = true;
 
-    
 
-
-// let url = document.querySelector('.url');
-let url = 'https://randomuser.me/api/?results=5';
+let inputUrl = document.querySelector('.url');
 let submitBtn = document.querySelector('.btn');
 let result = document.querySelector('#resultPrism');
+let jsonInput = document.querySelector('#jsonInput');
 
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener ('click', () => {
     let reqTypeValue = document.querySelector(`input[name='reqType']:checked`).value;
-    // console.log(url.value);
-
-    console.log(reqTypeValue);
-    
-   
-
-    getData(url, reqTypeValue).then((data) => {
-        displayData(data);
-         console.log(reqTypeValue);
-    }).catch((err) => {
-        console.log(err.message);
+    let url = inputUrl.value;
+    console.log(url);
+    if(url && url.trim().length){
         console.log(reqTypeValue);
-        loader.hidden = true;
-    })
+        if (reqTypeValue === 'GET'){
+            let getParam = {
+                method: reqTypeValue
+            }
+            getData(url, getParam).then((data) => {
+                displayData(data);
+                console.log(reqTypeValue);
+            }).catch((err) => {
+                console.log(err.message);
+                console.log(reqTypeValue);
+                loader.hidden = true;
+            })
+        } else {
+            let postParam = {
+                method: 'POST',
+                body: jsonInput.value,
+                headers: {
+                    "Content-type": 'application/json'
+                }
+            }
+            getData(url, postParam).then((data) => {
+                displayData(data);
+                console.log(reqTypeValue);
+            }).catch((err) => {
+                console.log(err.message);
+                console.log(reqTypeValue);
+                loader.hidden = true;
+            })
+        }
+    }
     
+      
 });
 
 
-async function getData(url, reqTypeValue){
+async function getData(url, fetchParam){
 
     loader.hidden = false;
 
-    let response = await fetch(url, {
-        method: reqTypeValue
-    });
+    let response = await fetch(url, fetchParam);
 
-    let data = await response.json();
+    let data = await response.text();
     return data;
 }
 
 
 function displayData(data){
-    console.log(data.results[0].email);
+    // console.log(data.results[0].email);
     console.log(data);
-    
         loader.hidden = true;
-
-        let a = JSON.stringify(data)
-        result.textContent = a;
+        result.innerHTML = data;
         console.log(result.textContent);
     }
     
 
 
 
+// let url = 'https://randomuser.me/api/?results=5';
+// let url = 'https://prash-sharma.github.io/animals.json';
+// let url = 'http://www.json-generator.com/api/json/get/coYoFCTehu?indent=2';
+
+
+let jsonObjEx = {
+    "title": "foo",
+    "body": "bar",
+    "id": 1
+}
