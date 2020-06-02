8650a6e4c7f88621c84d1ac3974cd399b5958a80
Prash Sharma <st.prashanta@gmail.com>
2020-06-02 18:20:28 +1000

    Hello world

---
+++ myreactapp-1/src/Helloworld.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Helloworld extends Component{
+    render(){
+       return <h1>Hello world, I'm reacting..</h1>
+    } 
+}
+
+export default Helloworld;
