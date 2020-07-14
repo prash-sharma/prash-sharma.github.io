057e11cee3b6aaee430c6c1c6742081fec0a90cf
prashant@appcurator.com <prashant@massive.co>
2020-07-15 09:48:12 +1000

    Updates

---
+++ precache-manifest.10e381b6bb841d762400d35d620abb4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c8568e3dc8ad151b00c609a6c83b52",
+    "url": "/router-test/index.html"
+  },
+  {
+    "revision": "f2b831fab789757ba775",
+    "url": "/router-test/static/css/main.c68b6bcb.chunk.css"
+  },
+  {
+    "revision": "c1660410ec977c6a45c6",
+    "url": "/router-test/static/js/2.4868d9e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/router-test/static/js/2.4868d9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b831fab789757ba775",
+    "url": "/router-test/static/js/main.85aa6d5d.chunk.js"
+  },
+  {
+    "revision": "fcfd3d84790b92a210d5",
+    "url": "/router-test/static/js/runtime-main.0605cfa4.js"
+  },
+  {
+    "revision": "26486aebe0b3a770d6d58f2260f03599",
+    "url": "/router-test/static/media/fire3.26486aeb.gif"
+  }
+]);
