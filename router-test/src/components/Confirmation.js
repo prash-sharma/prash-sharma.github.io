fd54867fe941c222be43f984b9e235616ef03477
prashant@appcurator.com <prashant@massive.co>
2020-07-23 12:34:19 +1000

    Fire

--- router-test/src/components/Confirmation.js
+++ router-test/src/components/Confirmation.js
@@ -6,15 +6,15 @@ export default function Confirmation(props) {
     let prompt = (
         <div className='promptDiv'>
             <div className='confirmationPrompt'>
-            <div>
-                {props.children}
+                <div>
+                    {props.children}
+                </div>
+                
+                <div className='promptBtns'>
+                    <button className='btnConfirm' onClick={props.onConfirm}>Delete</button>
+                    <button className='btnCancel' onClick={props.onCancel}>Cancel</button>
+                </div>
             </div>
-            
-            <div className='promptBtns'>
-                <button className='btnConfirm' onClick={props.onConfirm}>Delete</button>
-                <button className='btnCancel' onClick={props.onCancel}>Cancel</button>
-            </div>
-        </div>
         </div>
         
     )
